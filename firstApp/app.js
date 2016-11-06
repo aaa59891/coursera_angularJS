19824cc557ef2c2d146554fe57786e62a11a798e
Chong <aaa59891@gmail.com>
2016-11-06 21:21:12 +0800

    finished module1 solution

--- firstApp/app.js
+++ firstApp/app.js
@@ -1,10 +1,20 @@
 (function () {
   "use strict";
+  angular.module("NameCalculator", [])
+  .controller('NameCalculatorController', function ($scope, $chong) {
+    $scope.name = "";
+    $scope.totalValue = 0;
 
-  angular.module("myFirstApp", [])
-  .controller("myFirstController", function ($scope) {
-    var firstNum = $scope.firstNum = 3;
-    var secondNum = $scope.secondNum = 4;
-    $scope.total = firstNum + secondNum;
+    $scope.displayTotalValue = function () {
+      $scope.totalValue = calculateTotalValue($scope.name);
+    }
+    console.log($chong.annotate(NameCalculatorController));
+    function calculateTotalValue(string) {
+      var totalValue = 0;
+      for(var i = 0; i < string.length; i++){
+        totalValue += string.charCodeAt(i);
+      }
+      return totalValue;
+    }
   });
 })();
